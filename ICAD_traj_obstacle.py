e23b310ad3c24cb561370f1b02f07ab685a6031a
Feiyang Cai <feiyang.cai@vanderbilt.edu>
2019-08-09 09:03:41 -0500

    add traj icad

--- ICAD_traj_obstacle.py
+++ ICAD_traj_obstacle.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 data_list = []
 data_directory = "./2/obstacle/"
@@ -13,13 +14,13 @@ for filename in files:
     data_numpy = np.array(data)
     one_obstacle_list = np.empty([0, 10])
     for i in range(len(data_numpy)):
-        if data_numpy[i][6] <= 0 and len(one_obstacle_list)!=0:
+        if data_numpy[i][6] < 0 and len(one_obstacle_list)!=0:
             data_list.append(one_obstacle_list)
             one_obstacle_list = np.empty([0, 10])
-        if data_numpy[i][6] > 0:
-            one_obstacle_list.append(data_numpy[i])
-
-print(len(data_list))
+        if data_numpy[i][6] >= 0:
+            data_point = data_numpy[i].reshape(1,10)
+            one_obstacle_list = np.concatenate((one_obstacle_list, data_point))
+plt.plot(data_list[7][:,6])
 
 
 #%%
